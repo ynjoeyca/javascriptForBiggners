d4ba4d031d97a2d5b38715c983c1b3b731060011
ynjoeyca <32921799+ynjoeyca@users.noreply.github.com>
2019-11-05 20:30:29 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 // console.log("hello!");
 
 const name = "Yann";
-const sameSentence ="Loves Javascript!";
+const someSentence ="Loves Javascript!";
 console.log(name + " " + someSentence);
 console.log(`${name} really ${someSentence}`);
 
+const yourAge = 18;
+
+if(yourAge === 18) {
+  console.log("You're an adult now!") 
+} else if (yourAge < 18) {
+  console.log("Not yet huh");
+} else {
+  console.log("You can vote!");
+}
+
+const nationality = "Japanese";
+
+if(nationality === "Japanese") {
+  console.log("You can vote!");
+}
